f048d8f549f5d5963ed5b2184ffe7c34a407fe22
Christian Shaffer <calexandershaffer@gmail.com>
2023-09-16 16:31:33 -0400

    Complete basic calculator JS task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, element) => total + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1; 
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(baseNum, exponent) {
+	return Math.pow(baseNum, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let value = 1;
+	for (let i = num; i > 0; i--) {
+    value *= i;
+  }
+  return value;
 };
 
 // Do not edit below this line
